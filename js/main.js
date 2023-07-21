afbb0ccc9e19914583db4f5d2a112c937524c790
Aleksandr Murashov <alexandrosmur@gmail.com>
2023-07-21 18:48:56 +0300

    minor update 10

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ $(function(){
   $('.banner-section__slider').slick({
     dots: true,
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="/images/arrow-right.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
     responsive: [
       {
         breakpoint: 969,
@@ -42,7 +42,7 @@ $(function(){
     slidesToShow: 4,
     slidesToScroll: 1,
     prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="./images/arrow-black-left.svg" alt=""></button>',
-    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="../images/arrow-black-right.svg" alt=""></button>',
+    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="./images/arrow-black-right.svg" alt=""></button>',
     responsive: [
       {
         breakpoint: 1301,
