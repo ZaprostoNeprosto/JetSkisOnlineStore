3b3b74c55d3e3a3a47b32a12f676759cf0bead1a
Aleksandr Murashov <alexandrosmur@gmail.com>
2023-07-21 18:39:53 +0300

    minor update 10

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $(function(){
 
   $('.banner-section__slider').slick({
     dots: true,
-    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="/images/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="/images/arrow-right.svg" alt=""></button>',
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
     responsive: [
       {
         breakpoint: 969,
@@ -41,8 +41,8 @@ $(function(){
   $('.product-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="/images/arrow-black-left.svg" alt=""></button>',
-    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="/images/arrow-black-right.svg" alt=""></button>',
+    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
+    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
     responsive: [
       {
         breakpoint: 1301,
