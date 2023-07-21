a9aad045da99d3e59238d57d995a78ba2fe57537
Aleksandr Murashov <alexandrosmur@gmail.com>
2023-07-21 16:56:23 +0300

    minor update 9

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ $(function(){
 
   $('.banner-section__slider').slick({
     dots: true,
-    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
-    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
+    prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="../images/arrow-left.svg" alt=""></button>',
+    nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="../images/arrow-right.svg" alt=""></button>',
     responsive: [
       {
         breakpoint: 969,
@@ -41,8 +41,8 @@ $(function(){
   $('.product-slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
-    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="images/arrow-black-left.svg" alt=""></button>',
-    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="images/arrow-black-right.svg" alt=""></button>',
+    prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="../images/arrow-black-left.svg" alt=""></button>',
+    nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="../images/arrow-black-right.svg" alt=""></button>',
     responsive: [
       {
         breakpoint: 1301,
@@ -139,12 +139,19 @@ $(function(){
   
 });
 
-const input = document.querySelector('.card__input-search');
 function handleResize() {
-  if (window.matchMedia("(max-width: 700px)").matches) {
-    input.placeholder = 'Введите название магазина';
-  } else {
-    input.placeholder = '';
+  // Проверяем, есть ли элемент с id="page-id" на странице
+  const pageIdElement = document.getElementById("card__input-search");
+
+  if (pageIdElement) {
+    const input = document.getElementById("input");
+
+    if (window.matchMedia("(max-width: 700px)").matches) {
+      input.placeholder = 'Введите название магазина';
+    } else {
+      input.placeholder = '';
+    }
   }
 }
+
 window.addEventListener('resize', handleResize);
