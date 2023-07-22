501e8e54cd176ef66f3dcae0e3753cb7055449bd
Aleksandr Murashov <alexandrosmur@gmail.com>
2023-07-22 18:04:26 +0300

    new banner

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ $(function(){
 
   $('.banner-section__slider').slick({
     dots: true,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2500,
+    fade: true,
+    cssEase: 'linear',
     prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="images/arrow-left.svg" alt=""></button>',
     nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="images/arrow-right.svg" alt=""></button>',
     responsive: [
